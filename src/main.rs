30a5f943966ffbd13d0941967cd52808769bd106
Kieran Llarena <baymax709@gmail.com>
2025-12-01 11:39:35 -0500

    feat: helper function to get rotations from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+
+fn get_rotations(file_name: &str) -> Vec<String> {
+    let contents =
+        fs::read_to_string(file_name).expect(&format!("Could not find file {}", file_name));
+
+    contents
+        .split("\n")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.to_owned())
+        .collect()
+}
+
 fn main() {
     println!("Hello, world!");
 }
