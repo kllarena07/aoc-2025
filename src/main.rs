a92e77d3ae09d5ccae7e4f9231c1702e8468a279
Kieran Llarena <baymax709@gmail.com>
2025-12-01 15:24:10 -0500

    feat: helper to turn the dial

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,39 @@ fn get_rotations(file_name: &str) -> Vec<String> {
         .collect()
 }
 
+fn turn_dial(password: &mut u32, dial: &mut i32, direction: &char, clicks: i32) {
+    // L = -
+    // R = +
+    // dial cannot go below 0 or above 99
+    // if it goes below 0, it goes to 99
+    // if it goes above 99, it goes to 0
+
+    for _ in 0..clicks {
+        if direction == &'L' {
+            *dial -= 1;
+
+            if *dial < 0 {
+                *dial = 99;
+            }
+        } else if direction == &'R' {
+            *dial += 1;
+
+            if *dial > 99 {
+                *dial = 0;
+            }
+        }
+    }
+
+    // The actual password is the
+    // number of times the dial is left pointing
+    // at 0 after any rotation in the sequence.
+    if *dial == 0 {
+        *password += 1;
+    }
+
+    println!("The dial is rotated {direction}{clicks} to point at {dial}");
+}
+
 fn main() {
     println!("Hello, world!");
 }
