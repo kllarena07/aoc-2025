924342ac01b8c7bab613a71f54794fc100541d00
Kieran Llarena <baymax709@gmail.com>
2025-12-02 13:00:15 -0500

    chore: rm target directory

--- kieran/day-2/target/debug/day-2.d
+++
@@ -1,1 +0,0 @@
-/Users/kllarena/Documents/GitHub/aoc-2025/kieran/day-2/target/debug/day-2: /Users/kllarena/Documents/GitHub/aoc-2025/kieran/day-2/src/main.rs
--- kieran/day-2/target/debug/deps/day_2-d2a5de37055c7c3d.d
+++
@@ -1,5 +0,0 @@
-/Users/kllarena/Documents/GitHub/aoc-2025/kieran/day-2/target/debug/deps/day_2-d2a5de37055c7c3d.d: src/main.rs
-
-/Users/kllarena/Documents/GitHub/aoc-2025/kieran/day-2/target/debug/deps/day_2-d2a5de37055c7c3d: src/main.rs
-
-src/main.rs:
