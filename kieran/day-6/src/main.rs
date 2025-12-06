44c94d14010d9b96d63ab58b392537d2ff36d50b
Kieran Llarena <baymax709@gmail.com>
2025-12-06 02:48:38 -0500

    feat: solved day 6 part 1

--- kieran/day-6/src/main.rs
+++ kieran/day-6/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+
+mod part1;
+use crate::part1::Part1;
+
+fn get_worksheet(file_name: &str) -> Vec<String> {
+    let expect_msg = format!("Error: Could not find file {}.", file_name);
+    let contents = fs::read_to_string(file_name).expect(&expect_msg);
+
+    contents
+        .split("\n")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.to_owned())
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let worksheet = get_worksheet("./input.txt");
+
+    let part1 = Part1::default(worksheet);
+    let part1_solution = part1.solve();
+
+    println!("PART 1 Answer: {part1_solution}");
 }
---
+++ kieran/day-6/src/part1.rs
@@ -0,0 +1,58 @@
+pub struct Part1 {
+    pub arithmetic_data: Vec<Vec<String>>,
+}
+
+impl Part1 {
+    pub fn default(worksheet: Vec<String>) -> Self {
+        let arithmetic_data: Vec<Vec<String>> = worksheet
+            .iter()
+            .map(|line| {
+                line.split(" ")
+                    .filter(|char| !char.is_empty())
+                    .map(|char| char.to_owned())
+                    .collect()
+            })
+            .collect();
+
+        Self { arithmetic_data }
+    }
+
+    pub fn solve(&self) -> u128 {
+        let max_cols = self
+            .arithmetic_data
+            .iter()
+            .map(|row| row.len())
+            .max()
+            .unwrap();
+        let columns_data: Vec<Vec<String>> = (0..max_cols)
+            .map(|col_idx| {
+                self.arithmetic_data
+                    .iter()
+                    .rev()
+                    .filter_map(|row| row.get(col_idx).cloned())
+                    .collect()
+            })
+            .collect();
+
+        let mut answer = 0;
+
+        columns_data.iter().for_each(|line| {
+            let operation = line[0].as_str();
+            let start = 0 + ((operation == "*") as u128); // starting is 1 if operation is multiplication
+
+            let accumulator = &line[1..].iter().fold(start, |acc, s_num| {
+                let p_num = s_num.parse::<u128>().unwrap();
+
+                if operation == "*" {
+                    acc * p_num
+                } else {
+                    acc + p_num
+                }
+            });
+
+            answer += accumulator;
+        });
+
+        answer
+    }
+}
